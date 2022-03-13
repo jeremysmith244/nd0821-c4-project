215dec8b158153ca2936350b1b908b80bc48e235
Jeremy Smith <jeremysmith244@gmail.com>
2022-03-13 12:29:37 -0700

    added ingestion tracking

--- ingestion.py
+++ ingestion.py
@@ -23,13 +23,18 @@ def merge_multiple_dataframe():
     inpt_folder = Path(input_folder_path)
     files = list(inpt_folder.glob('*.csv'))
     summary = pd.DataFrame([])
+    ingested = []
     for file in files:
+        ingested.append(file.name)
         if summary.empty:
             summary = pd.read_csv(file)
         else:
             summary = summary.append(pd.read_csv(file), ignore_index=True)
     summary = summary.drop_duplicates()
     summary.to_csv(os.path.join(output_folder_path, 'finaldata.csv'))
+    with open(os.path.join(output_folder_path, 'ingestedfiles.txt'), 'w') as f:
+        for line in ingested:
+            f.write(line + '\n')
 
 if __name__ == '__main__':
     merge_multiple_dataframe()
