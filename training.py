36089a58da179c9258a9a33cabf52cec57190740
Jeremy Smith <jeremysmith244@gmail.com>
2022-03-13 12:50:39 -0700

    trained model

--- training.py
+++ training.py
@@ -2,6 +2,7 @@ from flask import Flask, session, jsonify, request
 import pandas as pd
 import numpy as np
 import pickle
+import joblib
 import os
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
@@ -12,21 +13,29 @@ import json
 with open('config.json','r') as f:
     config = json.load(f) 
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-model_path = os.path.join(config['output_model_path']) 
+dataset_csv_path = os.path.join(config['output_folder_path'], 'finaldata.csv') 
+model_path = os.path.join(config['output_model_path'], 'trainedmodel.joblib') 
 
 
 #################Function for training the model
 def train_model():
     
     #use this logistic regression for training
-    LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
-                    intercept_scaling=1, l1_ratio=None, max_iter=100,
-                    multi_class='warn', n_jobs=None, penalty='l2',
-                    random_state=0, solver='liblinear', tol=0.0001, verbose=0,
-                    warm_start=False)
+    log = LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
+                             intercept_scaling=1, l1_ratio=None, max_iter=100,
+                             multi_class='multinomial', n_jobs=None, penalty='l2',
+                             random_state=0, solver='lbfgs', tol=0.0001, verbose=0,
+                             warm_start=False)
     
     #fit the logistic regression to your data
+    data = pd.read_csv(dataset_csv_path)
+    X = data[['lastmonth_activity', 'lastyear_activity', 'number_of_employees']].values
+    y = data['exited'].values
+
+    log.fit(X, y)
     
     #write the trained model to your workspace in a file called trainedmodel.pkl
+    joblib.dump(log, model_path)
 
+if __name__ == "__main__":
+    train_model()
